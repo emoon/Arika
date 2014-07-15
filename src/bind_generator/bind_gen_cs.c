c3b8e1e2745b0a07d33e7e87949571617ee3efe2
Daniel Collin <daniel@collin.com>
2014-07-15 13:34:07 +0200

    Preparing for CS codegen

--- src/bind_generator/bind_gen_cs.c
+++ src/bind_generator/bind_gen_cs.c
@@ -1,2 +1,13 @@
-int dummy_cs() { return 0; }
+#include "bind_parser.h" 
+#include <stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int generate_cs(const char* filename, const BGFunction* func)
+{
+	(void)filename;
+	(void)func;
+
+	return -1;
+}
 
